fd7f6ba29ccc7403a9a49467c5353d22bf7062cb
mdrahathossenantor <md.rahathossenantor@gmail.com>
2024-02-28 22:41:25 +0600

    Server created

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
+import app from "./app";
+import { Server } from "http";
 
+const port: number = 5000;
+let server: Server;
+
+const bootstrap: Function = async () => {
+    server = app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+};
+bootstrap();
