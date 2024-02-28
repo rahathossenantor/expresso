c73bcc58a1f666c38941997325f524599b12163a
mdrahathossenantor <md.rahathossenantor@gmail.com>
2024-02-29 00:26:12 +0600

    get and post api created

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use(express.text());
+
+app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+    res.send(req.body);
 });
 
 export default app;
