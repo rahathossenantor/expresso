17033553395a30c023d91c692c62ade3f4bc2310
mdrahathossenantor <md.rahathossenantor@gmail.com>
2024-02-29 17:41:26 +0600

    Closing this repo

--- dist/app.js
+++ dist/app.js
@@ -7,10 +7,20 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+// routers
+const userRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 app.post("/", (req, res) => {
     res.send(req.body);
+});
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    res.json({
+        status: "success",
+        data: user
+    });
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,25 @@ const app = express();
 app.use(express.json());
 app.use(express.text());
 
+// routers
+const userRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
 });
 
 app.post("/", (req: Request, res: Response) => {
     res.send(req.body);
+});
+
+userRouter.post("/create-user", (req: Request, res: Response) => {
+    const user = req.body;
+    res.json({
+        status: "success",
+        data: user
+    });
 });
 
 export default app;
